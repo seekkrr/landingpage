faeb13165f46df6158547a6df8526ec8ba3f8cb6
Rishabh Maheshwari <rishphalod7@gmail.com>
2026-02-16 22:08:22 +0530

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/styles/index.css';
@@ -12,5 +13,6 @@ if (!rootElement) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
